b32dd1a1c2e4572b270e6c804f69a43fb04c4456
VBel3235 <Vlad95.0753@gmain.com>
2021-07-16 16:57:05 +1000

    Created a UICollectionViewFlowLayout + Diffable DataSource for the collectionView

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -9,25 +9,26 @@ import UIKit
 
 class FollowerListVC: UIViewController {
     
+    enum Section {
+        case main
+    }
+    
+    
+    var collectionView: UICollectionView!
     var userName: String!
+    
+    var followers: [Follower] = []
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.prefersLargeTitles = true
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+        configureDataSource()
         
-        NetworkManager.shared.getFollowers(username: userName, page: 1) { result in
-            
-            switch result {
-            case .success(let data):
-                print(data)
-            
-            case .failure(let error):
-                self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: error.rawValue, buttonTitle: "OK")
-            }
-            
- 
-        }
+     
 
         // Do any additional setup after loading the view.
     }
@@ -37,6 +38,66 @@ class FollowerListVC: UIViewController {
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
+    func configureCollectionView(){
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowlayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.resuseID)
+    }
+    
+    func configureViewController(){
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func createThreeColumnFlowlayout() -> UICollectionViewFlowLayout{
+        let width                       = view.bounds.width
+        let padding: CGFloat            = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth              = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth                   = availableWidth / 3
+        
+        let flowLayout                  = UICollectionViewFlowLayout()
+        flowLayout.sectionInset         = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize             = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+    
+    
+    
+    
 
-
+    func getFollowers(){
+        NetworkManager.shared.getFollowers(username: userName, page: 1) { result in
+            
+            switch result {
+            case .success(let data):
+                self.followers = data
+                self.updateData()
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: error.rawValue, buttonTitle: "OK")
+            }
+            
+ 
+        }
+    }
+    
+    func configureDataSource(){
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { collectionView, indexPath, follower in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.resuseID, for: indexPath) as! FollowerCell
+            cell.set(follower: follower)
+            return cell
+        })
+    }
+    
+    func updateData(){
+        var snapShot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapShot.appendSections([.main])
+        snapShot.appendItems(followers)
+        DispatchQueue.main.async {
+            self.dataSource.apply(snapShot, animatingDifferences: true)
+        }
+    
+    }
 }
